2f36254c81294ac410eb29a71d084f21a8d1c2b8
UnDer-7 <mateus7532@gmail.com>
2019-08-27 19:21:37 -0300

    Adding development url

--- lib/utils/api_util.dart
+++ lib/utils/api_util.dart
@@ -1,3 +1,7 @@
 class APIUtil {
+    /// Localhost URL
     static final String api = 'http://localhost:8080/api';
+
+    /// Development URL
+//    static final String api = 'https://cade-onibus-development.herokuapp.com/api';
 }

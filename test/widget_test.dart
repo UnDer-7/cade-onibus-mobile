b80534fc6ef730ccd8a549a136c8c732c1761d5e
UnDer-7 <mateus7532@gmail.com>
2019-09-02 22:42:37 -0300

    Fix tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cade_onibus_mobile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(CadeVan());
+    await tester.pumpWidget(CadeOnibus());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

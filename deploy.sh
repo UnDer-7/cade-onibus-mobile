03aa9a6df7f9d177227bd4364546e9533210f72c
UnDer-7 <mateus7532@gmail.com>
2019-09-29 04:01:27 -0300

    Add deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-ENV_FOLDER=$HOME/workspace/cade/cade_onibus/cade-onibus-mobile/lib/environments
+set -e
 
-echo "Movendo environment.dart para /tmp"
-mv "$ENV_FOLDER"/environment.dart /tmp
+PROJECT=$HOME/workspace/cade/cade_onibus/cade-onibus-mobile
+ENV_FOLDER="$PROJECT"/lib/environments
 
-echo "Renomeando environment_prod.dart"
-mv "$ENV_FOLDER"/environment_prod.dart environment.dart
+cd "$ENV_FOLDER"
+echo "Trocando environments"
+mv environment.dart /tmp
+mv environment_prod.dart environment.dart
 
+cd "$PROJECT"
+echo "Fazendo build do projeto"
+flutter clean
+flutter build appbundle
 
+cd "$ENV_FOLDER"
+echo "Trocando environments"
+mv environment.dart environment_prod.dart
+mv /tmp/environment.dart environment.dart
